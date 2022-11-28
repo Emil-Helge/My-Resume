b21e4ccd8c945c80a510b611fd7a20f0d8aa7631
Emil Helgesson <helgesson.emil.94@gmail.com>
2022-11-28 19:52:49 +0100

    Added further functionality and styling to header

---
+++ header.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', main);
+
+function main() {
+  addEventListeners()
+}
+
+function addEventListeners() {
+  const menuIcon = document.querySelector('.burger-menu');
+  menuIcon.addEventListener('click', toggleMenu);
+}
+
+function toggleMenu() {
+  const header = document.querySelector('header');
+  header.classList.toggle('h-56');
+
+  const menuIcon = document.querySelector('.burger-menu');
+  
+}
+
