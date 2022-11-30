5ef894b3937c0bb8178a1614dadc843d1ad9d77c
Emil Helgesson <helgesson.emil.94@gmail.com>
2022-11-30 18:55:08 +0100

    Added scrolling animation & created+scaled alt logo

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', main);
 
 function main() {
   addEventListeners()
+  scrollAnimation()
 }
 
 function addEventListeners() {
@@ -14,4 +15,19 @@ function toggleMenu() {
   header.classList.toggle('h-80');
 
   const menuIcon = document.querySelector('.burger-menu'); 
+}
+
+function scrollAnimation() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("scroll-animation-show");
+        } else {
+            entry.target.classList.remove("scroll-animation-show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".scroll-animation-hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 }
